14ac10653208080132af4907e0a765636d97cf6c
lorganisciak@gmail.com <lorganisciak@gmail.com>
2017-03-12 19:03:15 -0400

    Add redsocks solution

---
+++ redsocks/KattisSolution/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace KattisSolution
+{
+    internal class Program
+    {
+        public static readonly HashSet<long> tab = new HashSet<long>();
+
+        private static void Main(string[] args)
+        {
+
+           Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
+        }
+
+        public static void Solve(Stream stdin, Stream stdout)
+        {
+            var scanner = new OptimizedPositiveIntReader(stdin);
+            var writer = new BufferedStdoutWriter(stdout);
+            for (long i = 1; i < 50000; i++)
+            {
+                tab.Add(i * (i + 1));
+            }
+
+
+            var p = scanner.NextLong();
+            var q = scanner.NextLong();
+
+            while (q != 0)
+            {
+                var result = Calculate(p, q);
+                writer.Write(result);
+                writer.Write("\n");
+                p = scanner.NextLong();
+                q = scanner.NextLong();
+            }
+
+            writer.Flush();
+        }
+
+        public static string Calculate(long p, long q)
+        {
+            if (p == 0)
+                return "0 2";
+
+            var gcd = (long)BigInteger.GreatestCommonDivisor(p, q);
+            p = p / gcd;
+            q = q / gcd;
+
+            foreach (var l in tab)
+            {
+                if(l % q != 0)
+                    continue;
+
+                var x = l / q;
+                var newp = p * x;
+                if (tab.Contains(newp))
+                {
+                    var redS = (long)Math.Ceiling(Math.Sqrt(newp));
+                    var blackS = (long)Math.Ceiling(Math.Sqrt(l));
+
+                    return redS + " " + (blackS-redS);
+                }
+            }
+
+            return "impossible";
+        }
+    }
+
+    #region IO
+    public class OptimizedPositiveIntReader
+    {
+        private readonly StreamReader _reader;
+
+        public OptimizedPositiveIntReader(Stream inStream)
+        {
+            _reader = new StreamReader(inStream);
+        }
+
+        public OptimizedPositiveIntReader(StreamReader inStream)
+        {
+            _reader = inStream;
+        }
+
+        public int NextInt()
+        {
+            int c, result = 0;
+            var isInt = false;
+            while (!_reader.EndOfStream)
+            {
+                c = _reader.Read();
+
+                while (c >= '0' && c <= '9')
+                {
+                    result = result * 10 + c - '0';
+                    c = _reader.Read();
+                    isInt = true;
+                }
+
+                if (isInt)
+                    return result;
+            }
+
+            throw new Exception();
+        }
+
+        public long NextLong()
+        {
+            long c, result = 0;
+            var isInt = false;
+            while (!_reader.EndOfStream)
+            {
+                c = _reader.Read();
+
+                while (c >= '0' && c <= '9')
+                {
+                    result = result * 10 + c - '0';
+                    c = _reader.Read();
+                    isInt = true;
+                }
+
+                if (isInt)
+                    return result;
+            }
+
+            throw new Exception();
+        }
+
+    }
+
+    public class BufferedStdoutWriter : StreamWriter
+    {
+        public BufferedStdoutWriter(Stream outStream)
+            : base(new BufferedStream(outStream))
+        {
+        }
+    }
+
+    #endregion
+}
---
+++ redsocks/KattisSolution/TestGenerator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KattisSolution
+{
+    class TestGenerator
+    {
+        public static readonly HashSet<long> tab = new HashSet<long>();
+
+        private static void VerifyTests()
+        {
+            for (long i = 1; i < 50000; i++)
+            {
+                tab.Add(i * (i + 1));
+            }
+
+            StreamReader input = File.OpenText("redsocks2.in");
+/*            Stream ans = File.OpenRead("redsocks2.ans");
+            var scanner = new OptimizedPositiveIntReader(input);
+            var ansScanner = new LineReader(ans);
+            int j = 0;
+            while (scanner.HasNext())
+            {
+                j++;
+                var p = scanner.NextLong();
+                var q = scanner.NextLong();
+
+                var result = Program.Calculate(p, q);
+                var ans1 = ansScanner.Next();
+
+                if (result != ans1)
+                {
+                    Console.WriteLine(p + " " + q);
+                }
+            }*/
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+
+        private static void GeberateTests()
+        {
+            var input = File.OpenWrite("redsocks2.in");
+            var answers = File.OpenWrite("redsocks2.ans");
+            var answerWriter = new StreamWriter(answers);
+            var inputWriter = new StreamWriter(input);
+
+            var odpowiedzi = new HashSet<Tuple<long, long>>();
+
+            for (long total = 2; total <= 50000; total++)
+            {
+                for (long red = 0; red <= total; red++)
+                {
+                    if (red == 0 || red == 1)
+                    {
+                        answerWriter.Write("0 2");
+                        inputWriter.Write("0 4");
+                    }
+                    else
+                    {
+                        var licznik = red * (red - 1);
+                        var mianownik = (total) * (total - 1);
+                        var gcd = (long)BigInteger.GreatestCommonDivisor(licznik, mianownik);
+                        var t = Tuple.Create(licznik / gcd, mianownik / gcd);
+                        if (odpowiedzi.Contains(t))
+                        {
+                            continue;
+                        }
+
+                        odpowiedzi.Add(t);
+                        inputWriter.Write(t.Item1 + " " + t.Item2 + "\n");
+                        answerWriter.Write(red + " " + (total - red) + "\n");
+                    }
+
+                }
+                Console.WriteLine("total: " + total);
+            }
+
+            answerWriter.Flush();
+            inputWriter.Flush();
+            input.Close();
+            answers.Close();
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+    }
+}
