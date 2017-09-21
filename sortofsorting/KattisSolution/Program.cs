fb8d21b917d92f65d396537767b5133adfcf5ae6
lorganisciak@gmail.com <lorganisciak@gmail.com>
2017-09-21 08:18:07 -0400

    Add solution for sortofsorting

---
+++ sortofsorting/KattisSolution/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using KattisSolution.IO;
+
+namespace KattisSolution
+{
+    public class mycomp : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+           var r = x[0] - y[0] == 0 ? x[1]-y[1] : x[0] - y[0];
+            return r == 0 ? 1 : r;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
+        }
+
+        public static void Solve(Stream stdin, Stream stdout)
+        {
+            IScanner scanner = new LineReader(stdin);
+            // uncomment when you need more advanced reader
+            // IScanner scanner = new Scanner(stdin);
+            // IScanner scanner = new LineReader(stdin);
+            var writer = new BufferedStdoutWriter(stdout);
+
+            int n = 0;
+            int set = 0;
+
+            while ((n = scanner.NextInt()) != 0)
+            {
+                if(set!=0)
+                    writer.Write("\n");
+                var arr = new SortedSet<string>(new mycomp());
+                for (int i = 0; i < n; i++)
+                {
+                    arr.Add(scanner.Next());
+                }
+
+                foreach (var s in arr)
+                {
+                    writer.Write(s+"\n");
+                }
+                set++;
+
+            }
+
+            writer.Flush();
+        }
+    }
+}
---
+++ sortofsorting/KattisSolution/Tests/ProgramBasicTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace KattisSolution.Tests
+{
+    [Ignore("Uncoment this only when there's no data files")]
+    [TestFixture]
+    [Category("sample")]
+    public class ProgramBasicTest
+    {
+        [Test]
+        public void Solver_Should_ReturnIntMulitpliedBy5_When_SampleImplementation()
+        {
+            // Arrange
+            using (var stdin = new MemoryStream(Encoding.UTF8.GetBytes(" \n 12 ")))
+            using (var stdout = new MemoryStream())
+            {
+                // Act
+                Program.Solve(stdin, stdout);
+                var result = Encoding.UTF8.GetString(stdout.ToArray());
+
+                // Assert
+                Assert.That(result, Is.EqualTo("60\n"));
+            }
+        }
+    }
+}
